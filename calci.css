07a5ac266b8aa135b05b65241f95d2f9edf7f517
Aman Reddy Pundru <reddyaman77.ar@gmail.com>
2024-01-03 21:40:23 +0530

    Create calci.css

---
+++ calci.css
@@ -0,0 +1,36 @@
+/*
+const display = document.createElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+function calculate() {
+    display.value = eval(display.value);
+    
+}
+*/
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+function backspace(){
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
